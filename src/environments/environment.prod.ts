e8d6f3905437a25da28bf5e519d9222ac1f8efb8
Daryl110 <daryl.8220@gmail.com>
2020-08-29 19:13:55 -0500

    www_1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  nasa_api_config: {
+    url_base: 'https://api.nasa.gov',
+    api_key: 'WdxSnzI8uiRezkI9ceDzRpQe2QfVgXK8qDrdQnyN'
+  },
+  poke_api_url: 'https://pokeapi.co/api/v2'
 };
